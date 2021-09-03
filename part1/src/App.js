1f6ad1c5218204144c1ce38097bf0602991e674c
Michaela Wagner <mickywagner@gmail.com>
2021-09-03 10:39:12 -0600

    Part1 | Excercise 1.2 | Course information, step2

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,12 +7,18 @@ const Header = (props) => (
 )
 
 const Content = (props) => {
-  <>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p>
-    <p>{props.part3} {props.exercises3}</p>
-  </>
+  return (
+    <>
+      <Part part={props.part1} exercise= {props.exercises1}/>
+      <Part part={props.part2} exercise= {props.exercises2}/>
+      <Part part={props.part3} exercise= {props.exercises3} />
+    </>
+  )
 }
+
+const Part = (props) => (
+  <p>{props.part} {props.exercise}</p>
+)
 
 const Total = (props) => (
   <>
