0d2232bd9a00208e7254f429a8cdfaccfdbedec9
Michaela Wagner <mickywagner@gmail.com>
2021-09-03 17:03:53 -0600

    Part1 | Exercise 1.14 | anecdotes step3; display anecdote with the most votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
   const anecdotes = [
@@ -13,34 +13,59 @@ const App = () => {
 
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState(Array(7).fill(0))
+  const [mostVotes, setMostVotes] = useState(0)
 
   const getRandomNumber = () => Math.floor(Math.random() * anecdotes.length)
 
   const getNextAnecdote = () => {
     while (true) {
       const index = getRandomNumber();
-      
+
       if (index !== selected) {
         setSelected(index)
         return;
       }
-    }  
+    }
   }
 
   const addVote = () => {
     let copy = [...points]
     copy[selected] += 1
     setPoints(copy)
+    checkVotes(copy)
   }
 
-  
+  const checkVotes = (array) => {
+    let highestPoints = mostVotes;
+    let index = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > highestPoints) {
+        highestPoints = array[i];
+        index = i;
+      }
+    }
+
+    setMostVotes(index)
+  }
+
   return (
-    <div>
+    <div className="container">
+      <h1>Anecdote of the Day</h1>
       <p>
-        {anecdotes[selected]} - {points[selected]} votes
+        {anecdotes[selected]}
+        <span> - {points[selected]} votes</span> 
       </p>
-      <button onClick={addVote}>Vote</button>
-      <button onClick={getNextAnecdote}>Next anecdote</button>
+      
+      <div className="buttons">
+        <button onClick={addVote}>Vote</button>
+        <button onClick={getNextAnecdote}>Next anecdote</button>
+      </div>
+      <h2>Anecdote with the Most Votes</h2>
+      <p>
+        {anecdotes[mostVotes]}
+        <span> - {points[mostVotes]} votes</span>
+      </p>
     </div>
   )
 
