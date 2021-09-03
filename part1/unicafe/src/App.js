3f0a3a67dad7c1100686719a1f6b86778fa72e7e
Michaela Wagner <mickywagner@gmail.com>
2021-09-03 15:19:37 -0600

    Part1 | Exercise 1.11 | unicafe step6; display statistics in a table; add some style

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,20 +5,23 @@ import Statistics from "./Components/Statistics"
 
 
 const App = () => {
-  const [ good, setGood ] = useState(0)
-  const [ neutral, setNeutral ] = useState(0)
-  const [ bad, setBad ] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <div>
+    <div class="container">
       <Header />
 
-      <Button onClick={() => setGood(good + 1)} text="Good"/>
-      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral"/>
-      <Button onClick={() => setBad(bad + 1)} text="Bad"/>
+      <div class="buttons">
+        <Button onClick={() => setGood(good + 1)} text="Good" />
+        <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+        <Button onClick={() => setBad(bad + 1)} text="Bad" />
+      </div>
+
 
       <Statistics good={good} neutral={neutral} bad={bad} />
-      
+
     </div>
   )
 }
--- part1/unicafe/src/Components/Statistics.js
+++ part1/unicafe/src/Components/Statistics.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const StatisticsLine = ({ text, value }) => {
     return (
-        <div>{text}: {value}</div>
+        <>
+            <th>{text}</th>
+            <td>{value}</td>
+        </>
     )
 }
 
@@ -20,12 +23,36 @@ const Statistics = ({ good, neutral, bad }) => {
     }
     return (
         <>
-            <h2>Statistics</h2>
-            <StatisticsLine text="Good" value={good} />
-            <StatisticsLine text="Neutral" value={neutral} />
-            <StatisticsLine text="Bad" value={bad} />
-            <StatisticsLine text="All" value={total} />
-            <StatisticsLine text="Positive" value={percentGood} />
+            <table>
+                <thead>
+                    <tr>
+                    <th>Statistics</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <StatisticsLine text="Good" value={good} />
+                    </tr>
+                    <tr>
+                    <StatisticsLine text="Neutral" value={neutral} />
+                    </tr>
+                    <tr>
+                    <StatisticsLine text="Bad" value={bad} />
+                    </tr>
+                    <tr>
+                    <StatisticsLine text="All" value={total} />
+                    </tr>
+                    <tr>
+                    <StatisticsLine text="Positive" value={percentGood + 
+                    " %"} />
+                    </tr>
+                </tbody>
+            </table>
+
+        
+            
+           
+           
 
         </>
     )
