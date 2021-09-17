7b5319d2765d39eaff414c45701ebcc24db2f52d
Michaela Wagner <mickywagner@gmail.com>
2021-09-17 16:40:06 -0600

    part2 | 2.8 phonebook step3 | Add number entry

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,51 +1,55 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-      { name: 'Arto Hellas' }
-    ])
-    const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
+  const checkDuplicateEntry = (name) => {
+    return persons.findIndex((person) => person.name === name);
+  };
+  const addNewName = (e) => {
+    e.preventDefault();
 
-    const checkDuplicateEntry = (name) => {
-      return persons.findIndex(person => person.name === name) 
-    }
-    const addNewName = (e) => {
-      e.preventDefault()
-
-      if (checkDuplicateEntry(newName) > -1) {
-        alert(`${newName} is already added to the phonebook`)
-        setNewName('')
-        return;
-      }
-    
-      let newPerson = { name: newName}
-      setPersons(persons.concat(newPerson))
-      setNewName('')
+    if (checkDuplicateEntry(newName) > -1) {
+      alert(`${newName} is already added to the phonebook`);
+      setNewName("");
+      setNewNumber("");
+      return;
     }
 
-    const handleNameChange = (e) => {
-      setNewName(e.target.value)
-    }
+    let newPerson = { name: newName, number: newNumber };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+    setNewNumber("");
+  };
 
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
 
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
 
-    return(
-      <div>
-        <h2>Phonebook</h2>
-        <form onSubmit={addNewName}>
-          <label htmlFor="name">name: </label>
-          <input id="name" value={newName} onChange={handleNameChange}/>
-          <button type="submit">
-            add
-          </button>
-        </form>
-        <h2>Numbers</h2>
-        {persons.map(person => <div key={persons.indexOf(person)}>{person.name}</div>
-          )}
-      </div>
-    )
-}
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <label htmlFor="name">name:</label>
+        <input id="name" value={newName} onChange={handleNameChange} />
+        <label htmlFor="number">number: </label>
+        <input id="number" value={newNumber} onChange={handleNumberChange} />
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={persons.indexOf(person)}>{person.name} {person.number}</div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
-
