bd38897d63805ac26de53d0bfd3c1670b20cdef7
Michaela Wagner <mickywagner@gmail.com>
2021-09-17 16:59:20 -0600

    part2 | 2.9 phonebook step4 | Add filter functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const checkDuplicateEntry = (name) => {
     return persons.findIndex((person) => person.name === name);
@@ -26,27 +27,47 @@ const App = () => {
     setNewNumber("");
   };
 
-  const handleNameChange = (e) => {
-    setNewName(e.target.value);
-  };
-
-  const handleNumberChange = (e) => {
-    setNewNumber(e.target.value);
-  };
+  let filteredList = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <label htmlFor="filter">Filter shown by name: </label>
+      <input
+        id="filter"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+
+      <h2>Add new number</h2>
       <form onSubmit={addNewName}>
-        <label htmlFor="name">name:</label>
-        <input id="name" value={newName} onChange={handleNameChange} />
-        <label htmlFor="number">number: </label>
-        <input id="number" value={newNumber} onChange={handleNumberChange} />
+        <div>
+          <label htmlFor="name">name: </label>
+          <input
+            id="name"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="number">number: </label>
+          <input
+            id="number"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={persons.indexOf(person)}>{person.name} {person.number}</div>
+
+      {filteredList.map((person) => (
+        <div key={persons.indexOf(person)}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   );
