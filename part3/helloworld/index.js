17fbf2961fa6fa3ced8345ed8ddcfdd0924dab98
Michaela Wagner <mickywagner@gmail.com>
2021-10-19 16:41:51 -0600

    part3 | add error handling, put, delete functionality

--- part3/helloworld/index.js
+++ part3/helloworld/index.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const cors = require("cors");
-const { request, response, json } = require("express");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 
 const Note = require("./models/note");
 
@@ -68,36 +66,49 @@ app.post("/api/notes", (request, response) => {
   }
 
   const note = new Note({
-      content: body.content,
-      important: body.important,
-      date: new Date(),
-  })
-  
-  note.save().then(savedNote => {
-      response.json(savedNote)
-  })
+    content: body.content,
+    important: body.important,
+    date: new Date(),
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then(note => {
-      response.json(note)
-  })
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.put("api/notes/:id", (request, response) => {
-  let id = request.body.id;
-  let updatedNote = notes.find((note) => note.id === Number(request.body.id));
-  updatedNote.imporant = !notes.id.important;
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
-  let notes = [...notes, updatedNote];
-  response.send(updatedNote);
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -105,6 +116,18 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log("Error message: ", error.message);
+
+  if (error.name === "CastError") {
+    return response.status(404).send({ error: "malformed id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
