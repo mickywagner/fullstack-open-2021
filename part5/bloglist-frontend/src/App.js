3204d43592cb9d40ceadbc0838b84d624a65dd66
Michaela Wagner <mickywagner@gmail.com>
2021-11-30 09:31:56 -0700

    Fullstack Open | part5 | Save login info to local storage, implent logout

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -12,17 +12,30 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedInUser = JSON.parse(window.localStorage.getItem('blogAppLoginUser'))
+    if (loggedInUser) {
+      setUser(loggedInUser)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
-    event.preventDefault()
-    const { username, password } = event.target
-    const userToLogin = { username: username.value, password: password.value }
+    event.preventDefault();
+    const { username, password } = event.target;
+    const userToLogin = { username: username.value, password: password.value };
 
     try {
-      const response = await loginService.login(userToLogin)
-      setUser(response)
+      const response = await loginService.login(userToLogin);
+      setUser(response);
+      window.localStorage.setItem('blogAppLoginUser', JSON.stringify(response))
     } catch (exception) {
-      console.log(exception)
+      console.log(exception);
     }
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('blogAppLoginUser')
+    setUser(null)
   }
 
   return (
@@ -30,12 +43,15 @@ const App = () => {
       {user === null ? (
         <div>
           <h2>log in to application</h2>
-          <LoginForm handleLogin={handleLogin}/>
+          <LoginForm handleLogin={handleLogin} />
         </div>
       ) : (
         <div>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+
+          <p>{user.name} logged in  <button onClick={handleLogout}>Logout</button></p>
+         
+
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
